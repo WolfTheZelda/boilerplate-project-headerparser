9003d25af9282e469887112a391442ec439cbce1
Glitch (mercury-ludicrous-megaraptor) <none>
2020-03-20 06:35:10 +0000

    🐊🌱 Updated with Glitch

--- server.js
+++ server.js
@@ -24,6 +24,11 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+ 
+app.get("/api/whoami", function (req, res) {
+  res.json({ipaddress: req.headers['x-forwarded-for'].split(",")[0], language: req.headers['accept-language'], software: req.headers['user-agent']});
+});
+
 
 
 // listen for requests :)
